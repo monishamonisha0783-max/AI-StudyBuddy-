d724730cb882c5cc0036e6bc2ebeb3d377c57d7c
Monisha <monishamonisha0783@gmail.com>
2026-09-25 18:10:37 +0530

    Fix build errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Hero.css'
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import '../styles/Hero.css';
 import { GeneralContext } from '../context/GeneralContext';
 import Spinner from 'react-bootstrap/Spinner';
 
